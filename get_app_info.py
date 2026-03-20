d1f367bd608fe0e21e6806751dee0513cf2a6475
Albert Zeyer <albzey@gmail.com>
2026-03-20 12:50:50 +0100

    some restructure

--- get_app_info.py
+++ get_app_info.py
@@ -1,4 +1,8 @@
-from typing import Optional
+"""
+App info
+"""
+
+from typing import Optional, Any, Dict
 import os
 import sys
 import ast
@@ -9,6 +13,10 @@ mydir = os.path.dirname(__file__)
 
 
 def local_filename_from_url(filename: str) -> Optional[str]:
+    """
+    :param filename: a URL, which may be a file:// URL
+    :return: the local filename if the URL is a file:// URL, otherwise None
+    """
     if not filename.startswith("file://"):
         return None
     removestart = lambda s, t: s[len(t) :] if s.startswith(t) else s
@@ -17,86 +25,91 @@ def local_filename_from_url(filename: str) -> Optional[str]:
     return filename
 
 
-if sys.platform == "darwin":
+def get_app_info() -> Optional[Dict[str, Any]]:
+    """
+    :return: a dict with keys "appName", "windowTitle", "url", "idleTime" (in seconds), or None if failed to get info
+    """
+    if sys.platform == "darwin":
+        return _get_app_info_mac()
+    elif sys.platform == "win32":
+        return _get_app_info_win32()
+    else:
+        raise Exception(f"missing support for your platform {sys.platform}")
 
-    def get_app_info():
-        try:
-            ret = subprocess.Popen(
-                ["osascript", "-ss", mydir + "/mac/get_foregroundapp_info.scpt"], stdout=subprocess.PIPE
-            ).stdout.read()
-            ret = ret.strip().decode("utf-8")
-            (ret,) = re.match(r"^{(.*)}$", ret).groups()
-            appname, windowtitle = ast.literal_eval("(" + ret + ")")
-            idletime = (
-                subprocess.Popen(["sh", mydir + "/mac/get_idletime.sh"], stdout=subprocess.PIPE)
-                .stdout.read()
-                .strip()
-                .decode("utf-8")
-            )
-            idletime = float(idletime)
-            url = (
-                subprocess.Popen(["sh", mydir + "/mac/get_app_url.sh", appname, windowtitle], stdout=subprocess.PIPE)
-                .stdout.read()
-                .strip()
-                .decode("utf-8")
-            )
-            localfn = local_filename_from_url(url)
-            if localfn is not None:
-                m = re.match(r"(.*/Library/Containers/[^/]*/Data/[^/]*)(.*)", localfn)
-                if m and os.path.islink(m.groups()[0]):
-                    url = (
-                        "file://"
-                        + os.path.normpath(os.path.join(os.path.dirname(m.groups()[0]), os.readlink(m.groups()[0])))
-                        + m.groups()[1]
-                    )
-            return {"appName": appname, "windowTitle": windowtitle, "url": url, "idleTime": idletime}
-        except Exception as e:
-            print(e)
-            return None
 
-elif sys.platform == "win32":
+def _get_app_info_mac():
+    try:
+        ret = subprocess.Popen(
+            ["osascript", "-ss", mydir + "/mac/get_foregroundapp_info.scpt"], stdout=subprocess.PIPE
+        ).stdout.read()
+        ret = ret.strip().decode("utf-8")
+        (ret,) = re.match(r"^{(.*)}$", ret).groups()
+        appname, windowtitle = ast.literal_eval("(" + ret + ")")
+        idletime = (
+            subprocess.Popen(["sh", mydir + "/mac/get_idletime.sh"], stdout=subprocess.PIPE)
+            .stdout.read()
+            .strip()
+            .decode("utf-8")
+        )
+        idletime = float(idletime)
+        url = (
+            subprocess.Popen(["sh", mydir + "/mac/get_app_url.sh", appname, windowtitle], stdout=subprocess.PIPE)
+            .stdout.read()
+            .strip()
+            .decode("utf-8")
+        )
+        localfn = local_filename_from_url(url)
+        if localfn is not None:
+            m = re.match(r"(.*/Library/Containers/[^/]*/Data/[^/]*)(.*)", localfn)
+            if m and os.path.islink(m.groups()[0]):
+                url = (
+                    "file://"
+                    + os.path.normpath(os.path.join(os.path.dirname(m.groups()[0]), os.readlink(m.groups()[0])))
+                    + m.groups()[1]
+                )
+        return {"appName": appname, "windowTitle": windowtitle, "url": url, "idleTime": idletime}
+    except Exception as e:
+        print(e)
+        return None
 
-    def get_app_info():
-        try:
-            import win32gui
 
-            # TODO: or maybe win32gui.GetFocus() ?
-            hwnd = win32gui.GetForegroundWindow()
+def _get_app_info_win32():
+    try:
+        import win32gui
 
-            # Request privileges to enable "debug process", so we can
-            # later use PROCESS_VM_READ, retardedly required to
-            # GetModuleFileNameEx()
-            import win32security
-            import win32con
-            import win32process
-            import win32api
+        # TODO: or maybe win32gui.GetFocus() ?
+        hwnd = win32gui.GetForegroundWindow()
 
-            priv_flags = win32security.TOKEN_ADJUST_PRIVILEGES | win32security.TOKEN_QUERY
-            hToken = win32security.OpenProcessToken(win32api.GetCurrentProcess(), priv_flags)
-            # enable "debug process"
-            privilege_id = win32security.LookupPrivilegeValue(None, win32security.SE_DEBUG_NAME)
-            old_privs = win32security.AdjustTokenPrivileges(
-                hToken, 0, [(privilege_id, win32security.SE_PRIVILEGE_ENABLED)]
-            )
+        # Request privileges to enable "debug process", so we can
+        # later use PROCESS_VM_READ, retardedly required to
+        # GetModuleFileNameEx()
+        import win32security
+        import win32con
+        import win32process
+        import win32api
 
-            # Open the process, and query it's filename
-            processid = win32process.GetWindowThreadProcessId(hwnd)
-            pshandle = win32api.OpenProcess(
-                win32con.PROCESS_QUERY_INFORMATION | win32con.PROCESS_VM_READ, False, processid[1]
-            )
-            exename = win32process.GetModuleFileNameEx(pshandle, 0)
+        priv_flags = win32security.TOKEN_ADJUST_PRIVILEGES | win32security.TOKEN_QUERY
+        hToken = win32security.OpenProcessToken(win32api.GetCurrentProcess(), priv_flags)
+        # enable "debug process"
+        privilege_id = win32security.LookupPrivilegeValue(None, win32security.SE_DEBUG_NAME)
+        old_privs = win32security.AdjustTokenPrivileges(
+            hToken, 0, [(privilege_id, win32security.SE_PRIVILEGE_ENABLED)]
+        )
 
-            # clean up
-            win32api.CloseHandle(pshandle)
-            win32api.CloseHandle(hToken)
+        # Open the process, and query it's filename
+        processid = win32process.GetWindowThreadProcessId(hwnd)
+        pshandle = win32api.OpenProcess(
+            win32con.PROCESS_QUERY_INFORMATION | win32con.PROCESS_VM_READ, False, processid[1]
+        )
+        exename = win32process.GetModuleFileNameEx(pshandle, 0)
 
-            return {"appName": exename, "windowTitle": win32gui.GetWindowText(hwnd), "url": None, "idleTime": None}
+        # clean up
+        win32api.CloseHandle(pshandle)
+        win32api.CloseHandle(hToken)
 
-        except Exception as e:
-            print(e)
-            return None
+        return {"appName": exename, "windowTitle": win32gui.GetWindowText(hwnd), "url": None, "idleTime": None}
 
-    print("WARNING: win32 support is untested", file=sys.stderr)
+    except Exception as e:
+        print(e)
+        return None
 
-else:
-    raise Exception(f"missing support for your platform {sys.platform}")
